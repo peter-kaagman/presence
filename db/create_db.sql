f6ad66f581f8ac47e8bb90f6c5cd137ac7737480
Peter Kaagman <prjv.kaagman@gmail.com>
2023-12-12 10:26:10 +0000

    Minor changes

--- db/create_db.sql
+++ db/create_db.sql
@@ -1,6 +1,6 @@
 Create Table If Not Exists locaties(
     naam Text Not Null Primary Key,
-    aktief Integer Default 0,
+    aktief Integer Default 1,
     group_staf Text,
     group_lln Text,
     leerlingen_toegang Integer Default 0
@@ -8,13 +8,9 @@ Create Table If Not Exists locaties(
 Create Table If Not Exists medewerkers(
     upn Text Not Null,
     timestamp_aanwezig  Integer Default 0,
-    opmerking Text,
+    opmerking Text Default " ",
     opmerking_uit_agenda Integer Default 0,
-    agenda_prefix Text
+    agenda_prefix Text Default " "
 );
 
-Insert Into locaties ('naam','aktief','group_staf') values ('presence.test.writer',1,'presence-test');
-Insert Into locaties ('naam','aktief','group_staf') values ('presence.test.reader',1,'presence-test');
-Insert Into locaties ('naam','aktief','group_staf') values ('presence.test2.writer',1,'presence-test2');
-Insert Into medewerkers ('upn','timestamp_aanwezig','opmerking') values ('t.staf@atlascollege.nl','2023-11-13 08:11','ben er');
-Insert Into medewerkers ('upn','timestamp_aanwezig','opmerking') values ('p.kaagman@atlascollege.nl','2023-11-21 08:11','ben er');
+
